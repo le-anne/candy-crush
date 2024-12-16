1c4cac3f8e85bb8142a2b2538c8746a511e9084e
le-anne <leannevictoriaharris@gmail.com>
2024-12-16 14:16:12 -0800

    removed typo and reinstalled dependencies

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { resolve } from "node:path";
 import { readFileSync } from "node:fs";
 import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
-å
+
 
 // Export Vite configuration
 export default defineConfig(({ mode }) => {
