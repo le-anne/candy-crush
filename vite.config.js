d8995c67813f3a760fba9ca9684b83ad9dd9f95c
le-anne <leannevictoriaharris@gmail.com>
2024-12-16 14:13:40 -0800

    updated and removed structured clone

--- vite.config.js
+++ vite.config.js
@@ -2,9 +2,7 @@ import { resolve } from "node:path";
 import { readFileSync } from "node:fs";
 import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
-import structuredClone from "structured-clone";
-global.structuredClone = global.structuredClone || structuredClone;
-
+å
 
 // Export Vite configuration
 export default defineConfig(({ mode }) => {
